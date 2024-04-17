e688f53856230944a0a366763232aa51bb13ed2f
AsOne2020 <59190831+AsOne2020@users.noreply.github.com>
2024-04-17 11:47:50 +0800

    更新 astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://asone2020.github.io",
-  base: "/fuwari",
+  base: "/",
   integrations: [
     tailwind(),
     swup({
